cce6dfb0e682c378518e0c1127a68ae96263460b
Florian Saqipi <49369376+floriansaqipi@users.noreply.github.com>
2023-11-15 21:42:10 +0100

    Add project files.

---
+++ TCP_Socket_client_server_appliaction_WFA/Form1.cs
@@ -0,0 +1,10 @@
+namespace TCP_Socket_client_server_appliaction_WFA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TCP_Socket_client_server_appliaction_WFA/Program.cs
@@ -0,0 +1,17 @@
+namespace TCP_Socket_client_server_appliaction_WFA
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
