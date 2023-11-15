ca3b7dd490c0311531b01eefc50cab7140df2454
Florian Saqipi <49369376+floriansaqipi@users.noreply.github.com>
2023-11-15 21:45:07 +0100

    initial-TODOS

--- TCP_Socket_client_server_appliaction_WFA/Program.cs
+++ TCP_Socket_client_server_appliaction_WFA/Program.cs
@@ -8,6 +8,13 @@ namespace TCP_Socket_client_server_appliaction_WFA
         [STAThread]
         static void Main()
         {
+
+            //TODO: Implement server GUI
+            //TODO: Implement client GUI
+            //TODO: Implement basic message receive function server-side
+            //TODO: Implement basic message sending function client-side
+            //TODO: Add privileges to specified user
+            //TODO: Add read,write,execute
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
